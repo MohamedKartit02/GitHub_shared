09cf7ed1af0e1fde3d050d5c880c16680bbc215f
Mohamed Kartit <mkartit9@gmail.com>
2023-05-31 01:26:12 +0100

    add test_loop.js file

---
+++ practice/js/test_loop.js
@@ -0,0 +1,13 @@
+const timeToRepeat = 10;
+const Character = "F";
+
+function PrintCharacterTimes(){
+	let answer = "";
+	for (i = 0; i < timeToRepeat; i++){
+		answer += character;
+	}
+	console.log(answer);
+}
+
+PrintCharacterTime()
+
