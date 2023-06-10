31f960a8e8a2e1db53adf3fd015a01afdb926f70
Mohamed Kartit <mohamedka309@gmail.com>
2023-06-11 00:13:17 +0100

    Add the last exercices of the hours 4

--- Learn_C/Ex4/Ex4H4.c
+++ Learn_C/Ex4/Ex4H4.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int main()
 {
-	float dbl_num = 123.456;
+	double dbl_num = 123.456;
 
 	printf("this is the scientific value of the number 123.456 = %e.\n", dbl_num);
 	printf("This is the floating point value of the number 123.456 %f.\n", dbl_num);
---
+++ Learn_C/Ex4/Ex5H4.c
@@ -0,0 +1,11 @@
+/* This program print the scientific and flaoting format of 123.456*/
+
+#include <stdio.h>
+int main()
+{
+	char NumVal = '\n';
+
+	printf("This is the floating point value of the number %d.\n", NumVal);
+
+	return 0;
+}
