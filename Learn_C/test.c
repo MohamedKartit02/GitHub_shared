8bc006042fcd72fbd91631cd0bdc398af976a815
Mohamed Kartit <mkartit9@gmail.com>
2023-06-21 23:16:22 +0100

    add version 1 of the makeMDLinks file

---
+++ Learn_C/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 15;
+	printf("Octal: %o\n", n);
+	printf("Hexa: %x\n", n);
+	printf("decimal: %d\n", n);
+	printf("char: %c\n", n);
+	
+	return (0);
+}
